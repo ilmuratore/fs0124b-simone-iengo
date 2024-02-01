b9ba486065984d0ef5393b624e3b93a59491d26b
ilmuratore <iengo.simone\@gmail.com>
2024-02-01 16:39:27 +0100

    alcuni esercizi svolti

--- m1/s2/g4/js/D4.js
+++ m1/s2/g4/js/D4.js
@@ -4,10 +4,10 @@
 
 
 function areaRettangolo(l1,l2){
-    return 10 * 6
+    return l1 * l2
 }
 
-console.log(areaRettangolo())
+console.log(areaRettangolo(7,6))
 
 /* ESERCIZIO 2
  Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
@@ -15,29 +15,35 @@ console.log(areaRettangolo())
  la loro somma moltiplicata per tre.
 */
 
-function crazySum(a = 5 ,b = 5){
-  if(a === b){return a*3} 
+function crazySum(a,b){
+  if(a === b){return a+b*3} 
   else{return a+b}
 
-  
-  
 }
 
-console.log(crazySum())
+console.log(crazySum(5,2))
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
-function crazyDiff(){}
+function crazyDiff(a, b=19){
+if(a > 19){return (a-b)*3}
+else{return a-b}
+}
 
-
+console.log(crazyDiff(25))
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
+function boundary(n){
+ if(n >20 && n <=100 || n===400){return true}
+ else{return false}
+}
 
+console.log(boundary(400))
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -46,18 +52,25 @@ function crazyDiff(){}
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
  di 3 o di 7. (Suggerimento: usa l'operatore modulo)
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function check3and7(l){
+  if(l % 3 ){return true}
+  else if(l % 7){return true}
+  else{return false}
+}
+
+console.log(check3and7(1))
 
 /* ESERCIZIO 7
  Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+function reverseString(){}
 
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.

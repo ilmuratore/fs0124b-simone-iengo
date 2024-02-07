30377be4e816e518e0f809c44cdc629e9cbe0d45
Simone Iengo <iengo.simone@gmail.com>
2024-02-07 22:17:54 +0100

    progetto 2 caricato, altro metodo per fare todolist con js

--- m1/s3/g3/progetto 1/js/script.js
+++ m1/s3/g3/progetto 1/js/script.js
@@ -1,26 +1,22 @@
-// foglio script progetto to do list web app 
-
 
 
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-// logica di aggiunta elementi nella lista li dei promemoria //
+
 function addTask(){
     if(inputBox.value === ''){
-        alert("Davvero non hai nulla da fare?") // alert ok 
+        alert("Davvero non hai nulla da fare?")
     }
     else{
-        let li = document.createElement("li"); //creazione elemento li in html ok 
+        let li = document.createElement("li");
         li.innerHTML = inputBox.value; 
-        listContainer.appendChild(li); // inserimento in ul ok 
-        let span = document.createElement("span"); // inserimento span ok 
-        span.innerHTML = "\u00d7"; // x come "elemento grafico " ok
-        li.appendChild(span); // pure questo funziona
+        listContainer.appendChild(li); 
+        let span = document.createElement("span"); 
+        span.innerHTML = "\u00d7"; 
+        li.appendChild(span);
     }
-    inputBox.value = ""; // corretto 
+    inputBox.value = ""; 
 }
-
-//logica di modifica degli elementi nella lista 
 
 listContainer.addEventListener('click', function(e){
     if(e.target.tagName === 'LI'){
@@ -30,5 +26,3 @@ listContainer.addEventListener('click', function(e){
         e.target.parentElement.remove();
     }
 }, false)
-// non funziona cercare soluzione... forse cambiare il tutto con un ciclo for o forEach piuttosto che una funzione semplice. 
-// funziona da quando ho inserito  li e span in maiuscolo perche' ? da esaminare.... 

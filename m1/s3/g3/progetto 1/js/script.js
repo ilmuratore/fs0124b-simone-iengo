d2856dc0bbca293ec4cfd217fb185e0f50038689
Simone Iengo <iengo.simone@gmail.com>
2024-02-07 17:54:27 +0100

    finisched on first project on m1>s3>g3

--- m1/s3/g3/progetto 1/js/script.js
+++ m1/s3/g3/progetto 1/js/script.js
@@ -2,4 +2,33 @@
 
 
 
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+// logica di aggiunta elementi nella lista li dei promemoria //
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Davvero non hai nulla da fare?")
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
 
+//logica di modifica degli elementi nella lista 
+
+listContainer.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.add("checked");
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+    }
+}, false)
+// non funziona cercare soluzione... forse cambiare il tutto con un ciclo for o forEach piuttosto che una funzione semplice. 
+// funziona da quando ho inserito  li e span in maiuscolo perche' ? da esaminare.... 

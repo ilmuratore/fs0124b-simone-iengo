5bf328e48045752a99cd77411f73387e2e61428b
Simone Iengo <iengo.simone@gmail.com>
2024-03-04 02:16:36 +0100

    changelog in README

---
+++ m2/s2/progetto/src/js/homescript.js
@@ -0,0 +1,26 @@
+
+// tooggle button to show hero with video 
+// version 2.0
+
+let firstButton = document.getElementById('toggleButton1');
+let secondButton = document.getElementById('toggleButton2');
+let heroDiv = document.getElementsByClassName('hero')[0];
+
+firstButton.addEventListener('click', function(){
+    heroDiv.style.display = 'none';
+});
+
+secondButton.addEventListener('click', function(){
+    heroDiv.style.display = 'block';
+})
+
+
+// version 1.0
+// document.getElementById('toggleButton').addEventListener('click', function() {
+//    let heroDiv = document.getElementsByClassName('hero')[0];
+//    if (heroDiv.style.display === 'none') {
+//        heroDiv.style.display = 'block';
+//    } else {
+//        heroDiv.style.display = 'none';
+//    }
+//});

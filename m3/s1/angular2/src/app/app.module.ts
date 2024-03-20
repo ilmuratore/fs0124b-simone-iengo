226e00de4e721da581b4a92afccf7d2c771184f0
ha <iengo.simone@gmail.com>
2024-03-20 15:10:16 +0100

    coomit

---
+++ m3/s1/angular2/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ActivePostsComponent } from './active-posts/active-posts.component';
+import { InactivePostsComponent } from './inactive-posts/inactive-posts.component';
+import { PostDetailComponent } from './post-detail/post-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
+    PostDetailComponent,
+    NavbarComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ m3/s1/angular2/src/app/models/interface.ts
@@ -0,0 +1,9 @@
+export interface Interface {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+  tags: string[];
+  active: boolean;
+}
+

4fd6b38d5f48e2cc68584c24cc9ba3d18cada5a7
ha <iengo.simone@gmail.com>
2024-03-22 11:32:02 +0100

    created interface database

---
+++ m3/s1/progettoAngular1/src/app/database.ts
@@ -0,0 +1,11 @@
+export type Root = Root2[];
+
+export interface Root2 {
+  brand:string;
+  brandLogo:string;
+  model:string;
+  modelImage:string;
+  year:number;
+  price:number;
+  available:boolean;
+}

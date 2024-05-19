9f12091fdb1032cec53c41e8628d126f44a45cd0
ilmuratore <iengo.simone@gmail.com>
2024-05-19 23:58:27 +0200

    removed the start of javafx

--- backend/g20 verifica 4/GestionePrenotazioni/src/main/java/com/appGestione/GestionePrenotazioni/GestionePrenotazioniApplication.java
+++ backend/g20 verifica 4/GestionePrenotazioni/src/main/java/com/appGestione/GestionePrenotazioni/GestionePrenotazioniApplication.java
@@ -1,13 +1,11 @@
 package com.appGestione.GestionePrenotazioni;
 
-import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public abstract class GestionePrenotazioniApplication {
 
-	public abstract void start(Stage primaryStage);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionePrenotazioniApplication.class, args);

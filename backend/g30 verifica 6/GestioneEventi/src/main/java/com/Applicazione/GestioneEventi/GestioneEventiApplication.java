416a21f1c7e23ce386a6973ac67ef9d4403a4318
ilmuratore <iengo.simone@gmail.com>
2024-05-31 10:05:03 +0200

    main upload

---
+++ backend/g30 verifica 6/GestioneEventi/src/main/java/com/Applicazione/GestioneEventi/GestioneEventiApplication.java
@@ -0,0 +1,13 @@
+package com.Applicazione.GestioneEventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneEventiApplication.class, args);
+	}
+
+}
---
+++ backend/g30 verifica 6/GestioneEventi/src/test/java/com/Applicazione/GestioneEventi/GestioneEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Applicazione.GestioneEventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
